c19b5a87854b2dc96dac85ff445c02291bc38c1b
Dustin Maas <dmaas@flux.utah.edu>
2024-09-03 18:39:10 -0600

    Update gnuradio deploy to use uhd package and build.

--- bin/deploy-gnuradio.sh
+++ bin/deploy-gnuradio.sh
@@ -4,28 +4,78 @@ BINDIR=`dirname $0`
 ETCDIR=/local/repository/etc
 source $BINDIR/common.sh
 
-if [ -f $SRCDIR/gnuradio-setup-complete ]; then
-  echo "setup already ran; not running again"
-  exit 0
-fi
+echo "installing deps"
+sudo add-apt-repository -y ppa:ettusresearch/uhd
+sudo apt-get update
+sudo apt-get install -y libuhd-dev uhd-host
 
-# Get the emulab repo
-while ! wget -qO - http://repos.emulab.net/emulab.key | sudo apt-key add -
-do
-  echo Failed to get emulab key, retrying
-done
+sudo apt install \
+  cmake \
+  doxygen \
+  g++ \
+  gir1.2-gtk-3.0 \
+  git \
+  libad9361-dev \
+  libboost-all-dev \
+  libcodec2-dev \
+  libfftw3-dev \
+  libgmp-dev \
+  libgsl-dev \
+  libgsm1-dev \
+  libiio-dev \
+  liblog4cpp5-dev \
+  libqt5opengl5-dev \
+  libqwt-qt5-dev \
+  libsdl1.2-dev \
+  libsndfile1-dev \
+  libsoapysdr-dev \
+  libspdlog-dev \
+  libudev-dev \
+  libusb-1.0-0 \
+  libusb-1.0-0-dev \
+  libzmq3-dev \
+  pybind11-dev \
+  python3-click \
+  python3-click-plugins \
+  python3-gi \
+  python3-gi-cairo \
+  python3-jsonschema \
+  python3-lxml \
+  python3-mako \
+  python3-matplotlib \
+  python3-numpy \
+  python3-packaging \
+  python3-pygccxml \
+  python3-pyqt5 \
+  python3-pyqtgraph \
+  python3-qtpy \
+  python3-scipy \
+  python3-setuptools \
+  python3-sphinx \
+  python3-yaml \
+  python3-zmq \
+  soapysdr-tools
 
-while ! sudo add-apt-repository -y http://repos.emulab.net/powder/ubuntu/
-do
-  echo Failed to get johnsond ppa, retrying
-done
+cd $SRCDIR
+git clone --recursive https://github.com/gnuradio/volk.git
+cd volk
+mkdir build
+cd build
+cmake ../
+make -j`nproc`
+sudo make install
+sudo ldconfig
 
-while ! sudo apt-get update
-do
-  echo Failed to update, retrying
-done
+cd $SRCDIR
+git clone https://github.com/gnuradio/gnuradio.git
+cd gnuradio
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ../
+make -j`nproc`
+sudo make install
+sudo ldconfig
 
-sudo apt-get install -y --no-install-recommends libuhd-dev uhd-host gnuradio
-sudo uhd_images_downloader
+echo "building and installing GNU Radio"
 
 touch $SRCDIR/gnuradio-setup-complete
